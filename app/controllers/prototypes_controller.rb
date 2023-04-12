b9178761d666bddc99fce4756c82f34db8b752c5
kawabatakento <is311806323@gmail.com>
2023-04-12 21:26:20 +0900

    修正１

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -56,9 +56,8 @@ class PrototypesController < ApplicationController
   end
 
   def move_to_index
-    unless user_signed_in?
+    unless user_signed_in? 
       redirect_to action: :index
     end
   end
-  
 end
