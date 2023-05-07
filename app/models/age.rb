c5fe11bd7574d4eb064d2b1fbf2719feb76709fb
kawabatakento <is311806323@gmail.com>
2023-05-07 13:38:47 +0900

    調整

--- app/models/age.rb
+++ app/models/age.rb
@@ -3,8 +3,8 @@ class Age < ActiveHash::Base
     { id: 0, name: '10代' },
     { id: 1, name: '20代' },
     { id: 2, name: '30代' },
-    { id: 3, name: '30代' },
+    { id: 3, name: '40代' },
     { id: 4, name: '壮年' },
-    { id: 5, name: '老代' },
+    { id: 5, name: '老年' },
   ]
 end
